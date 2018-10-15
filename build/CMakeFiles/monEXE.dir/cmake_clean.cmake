3007cd34b81c226b711064147cce93eef02afde1
Nippow <hippolyte.favre@gmail.com>
2018-10-15 19:23:21 +0200

    ajout du build dans branchone

---
+++ build/CMakeFiles/monEXE.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/monEXE.dir/src/tableaux.cpp.o"
+  "CMakeFiles/monEXE.dir/src/moyenne_tab.cpp.o"
+  "CMakeFiles/monEXE.dir/src/saisie_tab.cpp.o"
+  "../bin/monEXE.pdb"
+  "../bin/monEXE"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/monEXE.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
